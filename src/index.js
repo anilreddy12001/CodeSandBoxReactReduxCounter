f2603455927921e287d945bf80d839b38345891d
anilreddy12001 <anilreddy12001@gmail.com>
2023-07-24 12:35:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
 function App() {
   const [count, setCount] = useState(0);
+   const [date, setDate] = useState(new Date());
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDate(new Date());
+    }, 1000)
 
+    return () => clearInterval(intervalId);
+  }, []);
+  
   return (
     <div className="App">
       <h1>App1 updated on 7dec2022, 12.20pm</h1>
       <h2>You clicked {count} times!</h2>
-
+<p className='clock'>
+      {date.toLocaleTimeString()}
+    </p>
       <button onClick={() => setCount(count - 1)}>Decrement</button>
       <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
