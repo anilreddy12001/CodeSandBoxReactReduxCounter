91c88dcd50b69ab61218996e854b1e5def1dd7fc
anilkumarrk <anilkumar.rk@axiscades.in>
2023-07-24 16:14:19 +0530

    display config values from aws amplify

--- src/index.js
+++ src/index.js
@@ -17,7 +17,8 @@ function App() {
   const REACT_APP_maxCountValue =process.env.REACT_APP_maxCountValue;
   return (
     <div className="App">
-      <h1>App1 updated on 24jul2023, 12.20pm</h1>
+      <h1>App1 updated on 24jul2023, 16:13hrs</h1>
+<p>REACT_APP_initialCounter: {REACT_APP_initialCounter},REACT_APP_maxCountValue: {REACT_APP_maxCountValue}</p>
       <h2>You clicked {count} times!</h2>
 <p className='clock'>
       {date.toLocaleTimeString()}
